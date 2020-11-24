9c0e38d3e9b8186a1e7f802521939f16d5574ea1
kim mun su <anstnsp@naver.com>
2020-11-24 12:46:22 +0900

    test

--- src/main/java/com/example/ssl/TestController.java
+++ src/main/java/com/example/ssl/TestController.java
@@ -1,6 +1,7 @@
 package com.example.ssl;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,9 +9,19 @@ public class TestController {
 
 
   @PostMapping("/anstnsp")
-  public String ssltest() {
+  public TestDto ssltest(@RequestBody String body) {
+    String response = body; 
+    TestDto dto = new TestDto();
+    dto.setLevel(1);
+    dto.setNum(33);
+    dto.setStr("스트링입니다.");
+
+    // Integer a = new Integer(100.123); 
+    // System.out.println("인티저값:"+a);
+    System.out.println("body:"+body);
+    System.out.println("response:"+ response);
     System.out.println("들어옴");
-    return "ssl success";
+    return dto;
   }
   
 }
---
+++ src/main/java/com/example/ssl/TestDto.java
@@ -0,0 +1,14 @@
+package com.example.ssl;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TestDto {
+
+  private int level; 
+  private String str; 
+  private int num; 
+
+}
